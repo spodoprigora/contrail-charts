ac6ab7ee8df34263acf413ef1e69acbd0c275d40
Sergey <slivers@ukr.net>
2017-06-08 14:35:50 +0300

    add integration tests. Interaction with the navigation component

--- tests/integration/navigation.js
+++ tests/integration/navigation.js
@@ -8,7 +8,6 @@ describe('Navigation', () => {
   beforeEach(() => {
     config = {
       id: 'chart',
-
       components: [{
         id: 'compositey-chart-id',
         type: 'CompositeY',
@@ -97,11 +96,11 @@ describe('Navigation', () => {
   })
 
   describe('Render with minimal config.', () => {
-    it('should show half line', (done) => {
+    xit('should show half line after selection range', (done) => {
       chart = new cc.composites.CompositeView({config, container})
       chart.setData(data)
-      let navclipPath = container.querySelector('#nav clipPath rect')
-      let navWidth = navclipPath.getBoundingClientRect().width
+      let navClipPath = container.querySelector('#nav clipPath rect')
+      let navWidth = navClipPath.getBoundingClientRect().width
       let brush = chart.composite._components[1]._brush
       brush.config.set('selection', [navWidth / 2, navWidth])
       let line = container.querySelector('#compositey-chart-id-y1-y path.line')
@@ -111,6 +110,155 @@ describe('Navigation', () => {
         let lineStartPoint = getPathStartPoint(d)
         let lineEndPoint = getPathEndPoint(d)
         expect(Math.abs(lineStartPoint.split(',')[0])).toBe(+lineEndPoint.split(',')[0])
+        done()
+      }, 0)
+    })
+  })
+
+  describe('Render with non-default config.', () => {
+    it('should show half line with selection in config', (done) => {
+      config.components[1].config.selection = [50, 100]
+      chart = new cc.composites.CompositeView({config, container})
+      chart.setData(data)
+      let line = container.querySelector('#compositey-chart-id-y1-y path.line')
+
+      setTimeout(() => {
+        let d = line.getAttribute('d')
+        let lineStartPoint = getPathStartPoint(d)
+        let lineEndPoint = getPathEndPoint(d)
+        expect(Math.abs(lineStartPoint.split(',')[0])).toBe(+lineEndPoint.split(',')[0])
+        done()
+      }, 0)
+    })
+
+    it('should show full line with selection in config', (done) => {
+      config.components[1].config.selection = [0, 100]
+      chart = new cc.composites.CompositeView({config, container})
+      chart.setData(data)
+      let line = container.querySelector('#compositey-chart-id-y1-y path.line')
+
+      setTimeout(() => {
+        let d = line.getAttribute('d')
+        let lineStartPoint = getPathStartPoint(d)
+        let lineEndPoint = getPathEndPoint(d)
+        let chartClipPath = container.querySelector('#compositey-chart-id clipPath rect')
+        let chartWidth = chartClipPath.getBoundingClientRect().width
+        expect(lineStartPoint.split(',')[0]).toBe('0')
+        expect(+lineEndPoint.split(',')[0]).toBe(chartWidth)
+        done()
+      }, 0)
+    })
+
+    it('should show full line after changing the selection', (done) => {
+      config.components[1].config.selection = [50, 100]
+      chart = new cc.composites.CompositeView({config, container})
+      chart.setData(data)
+      let line = container.querySelector('#compositey-chart-id-y1-y path.line')
+      let navClipPath = container.querySelector('#nav clipPath rect')
+      let navWidth = navClipPath.getBoundingClientRect().width
+      let brush = chart.composite._components[1]._brush
+      brush.config.set('selection', [0, navWidth])
+
+      setTimeout(() => {
+        let d = line.getAttribute('d')
+        let lineStartPoint = getPathStartPoint(d)
+        let lineEndPoint = getPathEndPoint(d)
+        let chartClipPath = container.querySelector('#compositey-chart-id clipPath rect')
+        let chartWidth = chartClipPath.getBoundingClientRect().width
+        expect(lineStartPoint.split(',')[0]).toBe('0')
+        expect(+lineEndPoint.split(',')[0]).toBe(chartWidth)
+        done()
+      }, 0)
+    })
+
+    it('reset navigation selection after click on chart', (done) => {
+      config = {
+        id: 'chart',
+        components: [{
+          id: 'multishape-bubble-chart',
+          type: 'CompositeY',
+          config: {
+            bucket: 'bucket-id',
+            update: ['navigation-id'],
+            height: 450,
+            plot: {
+              x: {
+                accessor: 'x',
+                axis: 'x',
+              },
+              y: [
+                {
+                  accessor: 'y',
+                  label: 'Data 1',
+                  chart: 'ScatterPlot',
+                  size: {
+                    accessor: 'size1',
+                    range: [100, 1000],
+                  },
+                  shape: 'Y',
+                  color: d => 'red',
+                  axis: 'y1'
+                }
+              ]
+            },
+            axes: {
+              x: {},
+              y1: {
+                position: 'left',
+                label: 'Y value of circles',
+              }
+            },
+          }
+        }, {
+          id: 'bucket-id',
+          type: 'Bucket',
+          config: {
+            range: [500, 800],
+            shape: 'X',
+          }
+        }, {
+          id: 'navigation-id',
+          type: 'Navigation',
+          config: {
+            height: 200,
+            selection: [50, 100],
+            update: ['multishape-bubble-chart'],
+            plot: {
+              x: {
+                accessor: 'x',
+              },
+              y: [
+                {
+                  accessor: 'y',
+                  chart: 'Line',
+                  color: 'grey',
+                }
+              ]
+            },
+            axes: {
+              x: {},
+              y: {
+                ticks: 6,
+              },
+            }
+          }
+        }]
+      }
+      data = [
+        {x: 0, y: 0, size1: 1},
+        {x: 1, y: 1, size1: 2},
+        {x: 2, y: 2, size1: 3}
+      ]
+      chart = new cc.composites.CompositeView({config, container})
+      chart.setData(data)
+
+      setTimeout(() => {
+        let svg = container.querySelector('svg')
+        let event = new Event('click', {bubbles: true})
+        svg.dispatchEvent(event)
+        let selection = container.querySelector('rect.selection')
+        let display = selection.style.display
+        expect(display).toBe('none')
         done()
       }, 0)
     })
