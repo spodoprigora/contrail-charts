08e222af58d3b63a595cf3180ed7a78e5f59e6e9
Sergey <slivers@ukr.net>
2017-06-09 18:19:29 +0300

    add tooltip tests

--- tests/specs/components/Tooltip.js
+++ tests/specs/components/Tooltip.js
@@ -10,191 +10,147 @@ describe('Tooltip', () => {
     config = {
       id: 'chart',
       components: [{
-        id: 'legend-id',
-        type: 'LegendPanel',
-        config: {
-          editable: {
-            color: true,
-            chart: true,
-          },
-        },
-      }, {
         id: 'compositey-chart-id',
         type: 'CompositeY',
         config: {
-          update: ['navigation'],
-          legend: 'legend-id',
-          crosshair: 'crosshair-id',
-          margin: {
-            left: 16,
-            right: 20,
-          },
-          height: 400,
+          duration: 0,
           chartTypes: {
-            y1: ['GroupedBar', 'StackedBar'],
-            y2: ['Line'],
+            y1: ['Line']
           },
           plot: {
             x: {
-              accessor: 'x',
-              labelFormatter: 'X Values',
-              axis: 'x',
+              accessor: 'x'
             },
             y: [
               {
                 accessor: 'a',
-                labelFormatter: 'Label A',
                 chart: 'GroupedBar',
                 axis: 'y1',
-                tooltip: 'default-tooltip',
-              }, {
-                accessor: 'b',
-                labelFormatter: 'Label B',
-                chart: 'GroupedBar',
-                axis: 'y1',
-                tooltip: 'default-tooltip',
-              }, {
-                accessor: 'c',
-                labelFormatter: 'Label C',
-                disabled: true,
-                chart: 'GroupedBar',
-                axis: 'y1',
-                tooltip: 'default-tooltip',
-              }, {
-                accessor: 'd',
-                labelFormatter: 'Label D',
-                color: '#d62728',
-                chart: 'Line',
-                axis: 'y2',
-                tooltip: 'default-tooltip',
-              }, {
-                accessor: 'e',
-                labelFormatter: 'Label E',
-                color: '#9467bd',
-                chart: 'Line',
-                axis: 'y2',
                 tooltip: 'default-tooltip',
               }
             ]
           },
           axes: {
-            x: {
-              scale: 'scaleLinear',
-              label: 'X',
-            },
-            y1: {
-              position: 'left',
-
-              domain: [-10, undefined],
-            },
-            y2: {
-              position: 'right',
-
-            }
+            x: {},
+            y1: {}
           },
-        },
-      }, {
-        type: 'Navigation',
-        config: {
-          id: 'navigation',
-          margin: {
-            left: 16,
-          },
-          height: 200,
-          selection: [75, 100],
-          update: ['compositey-chart-id'],
-          plot: {
-            x: {
-              accessor: 'x',
-              labelFormatter: 'X Values',
-            },
-            y: [
-              {
-                accessor: 'a',
-                labelFormatter: 'Label A',
-                chart: 'StackedBar',
-              }, {
-                accessor: 'b',
-                labelFormatter: 'Label B',
-                chart: 'StackedBar',
-              }, {
-                accessor: 'd',
-                labelFormatter: 'Label D',
-                chart: 'Line',
-              }
-            ]
-          },
-          axis: {
-            x: {
-              scale: 'scaleLinear',
-            },
-            y: {
-
-              ticks: 5,
-            },
-          }
         },
       }, {
         id: 'default-tooltip',
         type: 'Tooltip',
         config: {
-          title: {
-            accessor: 'x',
-          },
-          dataConfig: [
-            {
-              accessor: 'a',
-              labelFormatter: 'Label A',
-
-            }, {
-              accessor: 'b',
-              labelFormatter: 'Label B',
-
-            }, {
-              accessor: 'c',
-              labelFormatter: 'Label C',
-
-            }, {
-              accessor: 'd',
-              labelFormatter: 'Label D',
-
-            }, {
-              accessor: 'e',
-              labelFormatter: 'Label E',
-
-            }
-          ]
-        },
-      }, {
-        id: 'message-id',
-        type: 'Message',
-        config: {
-        }
-      }, {
-        id: 'crosshair-id',
-        type: 'Crosshair',
-        config: {
-          tooltip: 'default-tooltip',
+          title: {accessor: 'x'},
+          dataConfig: [{accessor: 'a'}]
         }
       }]}
     data = [
-      {x: 0, a: 2, b: 1, c: 2, d: 1, e: 3},
-      {x: 1, a: 3, b: 3, c: 4, d: 1, e: 4},
-      {x: 2, a: 5, b: 4, c: 1, d: 2, e: 5},
-      {x: 3, a: 1, b: 6, c: 2, d: 3, e: 2},
-      {x: 4, a: 2, b: 0, c: 2, d: 4, e: 1},
-      {x: 5, a: 0, b: 3, c: 0, d: 5, e: 1}
+      {x: 0, a: 2},
+      {x: 1, a: 3},
+      {x: 2, a: 5},
+      {x: 3, a: 1},
+      {x: 4, a: 2}
     ]
   })
 
   afterEach(() => {
-    //while (container.firstChild) { container.firstChild.remove() }
+    while (container.firstChild) { container.firstChild.remove() }
   })
 
   describe('Render with minimal config.', () => {
-    fit('123', () => {
+    it('show tooltip on mousemove over bars', (done) => {
       chart = new cc.composites.CompositeView({config, container})
       chart.setData(data)
+      let bar = container.querySelector('rect.bar')
+
+      observe('attr', bar, 'height', () => {
+        let barRect = bar.getBoundingClientRect()
+        let eventPoint = {x: Math.ceil(barRect.left), y: Math.ceil(barRect.top)}
+        let event = new MouseEvent('mousemove', {
+          bubbles: true,
+          clientX: eventPoint.x,
+          clientY: eventPoint.y
+        })
+        bar.dispatchEvent(event)
+        let tooltip = container.querySelector('.tooltip.active')
+        expect(tooltip).not.toBeNull()
+        done()
+      })
+    })
+  })
+
+  describe('cheking correctnes tooltip position for Grouped Bar', () => {
+    it('vertical top', (done) => {
+      // vertical is default position
+      chart = new cc.composites.CompositeView({config, container})
+      chart.setData(data)
+      let margin = 10
+      let firstBar = container.querySelector('rect.bar')
+
+      observe('attr', firstBar, 'height', () => {
+        let firstBarRect = firstBar.getBoundingClientRect()
+        let eventPoint = {x: Math.ceil(firstBarRect.left), y: Math.ceil(firstBarRect.top)}
+        let event = new MouseEvent('mousemove', {
+          bubbles: true,
+          clientX: eventPoint.x,
+          clientY: eventPoint.y
+        })
+        firstBar.dispatchEvent(event)
+        let tooltip = container.querySelector('.tooltip-content')
+        let tooltipRect = tooltip.getBoundingClientRect()
+
+        expect(tooltipRect.top).toBeCloseTo(event.offsetY - tooltipRect.height - margin)
+        expect(tooltipRect.left).toBeCloseTo(event.offsetX - tooltipRect.width / 2)
+        done()
+      })
+    })
+
+    it('vertical bottom', (done) => {
+      // tooltip for higest bar
+      chart = new cc.composites.CompositeView({config, container})
+      chart.setData(data)
+      let margin = 10
+      let higestBar = container.querySelectorAll('rect.bar')[2]
+
+      observe('attr', higestBar, 'height', () => {
+        let higestBarRect = higestBar.getBoundingClientRect()
+        let eventPoint = {x: Math.ceil(higestBarRect.left), y: Math.ceil(higestBarRect.top)}
+        let event = new MouseEvent('mousemove', {
+          bubbles: true,
+          clientX: eventPoint.x,
+          clientY: eventPoint.y
+        })
+        higestBar.dispatchEvent(event)
+        let tooltip = container.querySelector('.tooltip-content')
+        let tooltipRect = tooltip.getBoundingClientRect()
+
+        expect(tooltipRect.top).toBeCloseTo(event.offsetY + margin)
+        expect(tooltipRect.left).toBeCloseTo(event.offsetX - tooltipRect.width / 2)
+        done()
+      })
+    })
+
+    it('vertical center', (done) => {
+      config.components[0].config.height = 150
+      chart = new cc.composites.CompositeView({config, container})
+      chart.setData(data)
+      let firstBar = container.querySelector('rect.bar')
+
+      observe('attr', firstBar, 'height', () => {
+        let firstBarRect = firstBar.getBoundingClientRect()
+        let eventPoint = {x: Math.ceil(firstBarRect.left), y: Math.ceil(firstBarRect.top)}
+        let event = new MouseEvent('mousemove', {
+          bubbles: true,
+          clientX: eventPoint.x,
+          clientY: eventPoint.y
+        })
+        firstBar.dispatchEvent(event)
+        let tooltip = container.querySelector('.tooltip-content')
+        let tooltipRect = tooltip.getBoundingClientRect()
+
+        expect(tooltipRect.top).toBeCloseTo(event.offsetY - tooltipRect.height / 2)
+        expect(tooltipRect.left).toBeCloseTo(event.offsetX - tooltipRect.width / 2)
+        done()
+      })
     })
   })
 })
